905e82833cd14ff6a0a75bff35a3436cceb710ba
MephistoDevelop <inesherald@gmail.com>
2020-03-29 05:12:46 -0600

    Finished Desktop version

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useSate } from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 
@@ -11,16 +11,12 @@ export const RecipeDetails = () => {
     const objArr = [];
     for (let i = 1; i < 19; i += 1) {
       if (meal[`strIngredient${i}`] !== '') {
-        objArr.push(<div className="d-flex col">
-          <div className="w-50">
-            Ingredient:
-            {meal[`strIngredient${i}`]}
-          </div>
-          <div>
-            {' '}
-            {meal[`strMeasure${i}`]}
-          </div>
-        </div>);
+        objArr.push(
+          <tr>
+            <td>{meal[`strIngredient${i}`]}</td>
+            <td>{meal[`strMeasure${i}`]}</td>
+          </tr>
+        );
       }
       // console.log(`Hey: ${JSON.stringify(meal)}${meal[`strIngredient${i}`]} , ${meal[`strMeasure${i}`]}`);
     }
@@ -29,30 +25,39 @@ export const RecipeDetails = () => {
 
   return (
     <div>
-      <img className="img-thumbnail random-img" src={meal.strMealThumb} />
-      <div>
-        Meal :
-        <br />
-        {meal.strMeal}
+      <div id="header-container" className="d-flex justify-content-center mt-5">
+        <div id="image-details-container" className="mr-5">
+          <img id="img-details" src={meal.strMealThumb} />
+          <div id="meal-name">
+            {meal.strMeal}
+          </div>
+          <div id="meal-category">
+            Category:
+            {meal.strCategory}
+          </div>
+        </div>
+        <div id="ingredients-container" >
+          <div className="d-flex col">
+            <table>
+              <tr>
+                <th>Ingredient</th>
+                <th>Measure</th>
+              </tr>
+              {getIngredients(meal)}
+            </table>
+          </div>
+          <div>
+          </div>
+        </div>
       </div>
-      <div>
-        Category:
-        <br />
-        {meal.strCategory}
-      </div>
-      <br />
-      {getIngredients(meal)}
-      <div>
-        Intrucions:
-        <br />
+      <div id="instructions-container">
+        <p>Instrucions:</p>
         {' '}
         {meal.strInstructions}
       </div>
-      <div className="mt-5"><ReactPlayer url={meal.strYoutube} /></div>
-      <br />
-      <br />
-      <br />
-      {/* HelLo Datils {JSON.stringify(location.state.meal)} */}
+      <div id="video-container" className="mt-5">
+        <ReactPlayer url={meal.strYoutube} />
+      </div>
     </div>
   );
 };
--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,14 +1,18 @@
 import { useEffect, useState } from 'react';
 import React from 'react';
 import axios from 'axios';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 
 let mealsArr = [];
+let meal = null;
+let mealSearched = [];
 
 export const RecipesList = (props) => {
   const [rendered, setRendered] = useState(false);
+  const [redirect, setRedirect] = useState(false);
   const location = useLocation();
   const categoryObj = location.state.category;
+  const history = useHistory();
 
   const fetch = async (category) => await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`).then((result) => {
     //  alert(JSON.stringify(result.data.meals)+'\n Category: '+category);
@@ -19,18 +23,31 @@ export const RecipesList = (props) => {
     setRendered(true);
   });
 
+  const fetchMeal = async (meal) => await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal.idMeal}`).then((result) => {
+    mealSearched = [];
+    mealSearched.push(result.data.meals[0]);
+  });
+
+  const onClick = async (mealObj) => {
+    await fetchMeal(mealObj);
+    // alert(JSON.stringify(mealSearched));
+    console.log(`mealSearched: ${JSON.stringify(mealSearched[0])}`);
+    setRedirect(true);
+  };
   const setMeals = (meals) => {
     const divsArr = [];
     if (rendered) {
       for (let i = 0; i < meals.length; i += 1) {
-        divsArr.push(<div name={meals[i].idMeal}>
-          <div>
-            {' '}
-            <img className="img-thumbnail random-img" src={meals[i].strMealThumb} />
-          </div>
-          <div>
-            "Name "{meals[i].strMeal}</div>
-        </div>);
+        divsArr.push(
+          <div className="details-container d-flex m-4" name={meals[i].idMeal} onClick={() => onClick(meals[i])}>
+            <div>
+              {' '}
+              <img className="img-list" src={meals[i].strMealThumb} />
+            </div>
+            <div className="d-flex align-self-center ml-3">
+              <p>{meals[i].strMeal}</p>
+            </div>
+          </div>);
       }
       return (divsArr);
     }
@@ -39,12 +56,16 @@ export const RecipesList = (props) => {
 
   useEffect(() => {
     const result = fetch(categoryObj.strCategory);
-    console.log(`IM result from Fetch:: ${JSON.stringify(mealsArr)}`);
+    // console.log(`IM result from Fetch:: ${JSON.stringify(mealsArr)} `);
   }, []);
 
   return (
     <div>
       {setMeals(mealsArr)}
+      {redirect ? history.replace({
+        pathname: './details',
+        state: { meal: mealSearched[0] }
+      }) : null}
     </div>
   );
 };

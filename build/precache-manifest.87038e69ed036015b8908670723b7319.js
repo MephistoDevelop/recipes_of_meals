5937e980caec6efac53fee227244784ed9311038
MephistoDevelop <inesherald@gmail.com>
2020-03-30 03:20:25 -0600

    Deploy to heroku

---
+++ build/precache-manifest.87038e69ed036015b8908670723b7319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fc8d098abefeaf5439054c98a20aa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96989b8c3959a24f574d",
+    "url": "/static/css/main.2817a54a.chunk.css"
+  },
+  {
+    "revision": "b6973b4fc1418f49519b",
+    "url": "/static/js/2.298f34fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.298f34fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96989b8c3959a24f574d",
+    "url": "/static/js/main.1fa3026b.chunk.js"
+  },
+  {
+    "revision": "329523396de7acb0c5b5",
+    "url": "/static/js/runtime-main.061ebd0b.js"
+  }
+]);
